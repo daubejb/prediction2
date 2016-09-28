f3cee3aef81dc7a9b23595a8bc16882a8741212f
daubejb <daubejb@gmail.com>
2016-09-28 12:37:32 -0400

    replace png with vector images where possible

--- app/src/main/java/com/daubedesign/prediction2/ImageAdapter.java
+++ app/src/main/java/com/daubedesign/prediction2/ImageAdapter.java
@@ -51,19 +51,19 @@ public class ImageAdapter extends BaseAdapter {
 
     // references to our images
     private Integer[] mThumbIds = {
-            R.drawable.clubs_as, R.drawable.hearts_as, R.drawable.spades_as, R.drawable.diamonds_as
-//            R.drawable.clubs_2, R.drawable.hearts_2, R.drawable.spades_2, R.drawable.diamonds_2,
-//            R.drawable.clubs_3, R.drawable.hearts_3, R.drawable.spades_3, R.drawable.diamonds_3
-//            R.drawable.clubs_4, R.drawable.hearts_4, R.drawable.spades_4, R.drawable.diamonds_4,
-//            R.drawable.clubs_5, R.drawable.hearts_5, R.drawable.spades_5, R.drawable.diamonds_5,
-//            R.drawable.clubs_6, R.drawable.hearts_6, R.drawable.spades_6, R.drawable.diamonds_6,
-//            R.drawable.clubs_7, R.drawable.hearts_7, R.drawable.spades_7, R.drawable.diamonds_7,
-//            R.drawable.clubs_8, R.drawable.hearts_8, R.drawable.spades_8, R.drawable.diamonds_8,
-//            R.drawable.clubs_9, R.drawable.hearts_9, R.drawable.spades_9, R.drawable.diamonds_9,
-//            R.drawable.clubs_10, R.drawable.hearts_10, R.drawable.spades_10, R.drawable.diamonds_10,
-//            R.drawable.clubs_j, R.drawable.hearts_j, R.drawable.spades_j, R.drawable.diamonds_j,
-//            R.drawable.clubs_q, R.drawable.hearts_q, R.drawable.spades_q, R.drawable.diamonds_q,
-//            R.drawable.clubs_k, R.drawable.hearts_k, R.drawable.spades_k, R.drawable.diamonds_k,
+            R.drawable.clubs_a, R.drawable.hearts_a, R.drawable.spades_a, R.drawable.diamonds_a,
+            R.drawable.clubs_2, R.drawable.hearts_2, R.drawable.spades_2, R.drawable.diamonds_2,
+            R.drawable.clubs_3, R.drawable.hearts_3, R.drawable.spades_3, R.drawable.diamonds_3,
+            R.drawable.clubs_4, R.drawable.hearts_4, R.drawable.spades_4, R.drawable.diamonds_4,
+            R.drawable.clubs_5, R.drawable.hearts_5, R.drawable.spades_5, R.drawable.diamonds_5,
+            R.drawable.clubs_6, R.drawable.hearts_6, R.drawable.spades_6, R.drawable.diamonds_6,
+            R.drawable.clubs_7, R.drawable.hearts_7, R.drawable.spades_7, R.drawable.diamonds_7,
+            R.drawable.clubs_8, R.drawable.hearts_8, R.drawable.spades_8, R.drawable.diamonds_8,
+            R.drawable.clubs_9, R.drawable.hearts_9, R.drawable.spades_9, R.drawable.diamonds_9,
+            R.drawable.clubs_10, R.drawable.hearts_10, R.drawable.spades_10, R.drawable.diamonds_10,
+            R.drawable.clubs_j, R.drawable.hearts_j, R.drawable.spades_j, R.drawable.diamonds_j,
+            R.drawable.clubs_q, R.drawable.hearts_q, R.drawable.spades_q, R.drawable.diamonds_q,
+            R.drawable.clubs_k, R.drawable.hearts_k, R.drawable.spades_k, R.drawable.diamonds_k,
 
     };
 }
