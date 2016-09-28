06e31ee3034fb046c938a1476d140a709cf8ff32
daubejb <daubejb@gmail.com>
2016-09-27 22:30:42 -0400

    add gridview to select a prediction - added four new small images

---
+++ app/src/main/java/com/daubedesign/prediction2/ImageAdapter.java
@@ -0,0 +1,70 @@
+package com.daubedesign.prediction2;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import edu.cmu.pocketsphinx.demo.R;
+
+/**
+ * Created by root on 9/27/16.
+ */
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(171, 239));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+
+    // references to our images
+    private Integer[] mThumbIds = {
+            R.drawable.clubs_as, R.drawable.hearts_as, R.drawable.spades_as, R.drawable.diamonds_as
+//            R.drawable.clubs_2, R.drawable.hearts_2, R.drawable.spades_2, R.drawable.diamonds_2,
+//            R.drawable.clubs_3, R.drawable.hearts_3, R.drawable.spades_3, R.drawable.diamonds_3
+//            R.drawable.clubs_4, R.drawable.hearts_4, R.drawable.spades_4, R.drawable.diamonds_4,
+//            R.drawable.clubs_5, R.drawable.hearts_5, R.drawable.spades_5, R.drawable.diamonds_5,
+//            R.drawable.clubs_6, R.drawable.hearts_6, R.drawable.spades_6, R.drawable.diamonds_6,
+//            R.drawable.clubs_7, R.drawable.hearts_7, R.drawable.spades_7, R.drawable.diamonds_7,
+//            R.drawable.clubs_8, R.drawable.hearts_8, R.drawable.spades_8, R.drawable.diamonds_8,
+//            R.drawable.clubs_9, R.drawable.hearts_9, R.drawable.spades_9, R.drawable.diamonds_9,
+//            R.drawable.clubs_10, R.drawable.hearts_10, R.drawable.spades_10, R.drawable.diamonds_10,
+//            R.drawable.clubs_j, R.drawable.hearts_j, R.drawable.spades_j, R.drawable.diamonds_j,
+//            R.drawable.clubs_q, R.drawable.hearts_q, R.drawable.spades_q, R.drawable.diamonds_q,
+//            R.drawable.clubs_k, R.drawable.hearts_k, R.drawable.spades_k, R.drawable.diamonds_k,
+
+    };
+}
+
--- app/src/main/java/com/daubedesign/prediction2/SelectCardActivity.java
+++ app/src/main/java/com/daubedesign/prediction2/SelectCardActivity.java
@@ -3,7 +3,10 @@ package com.daubedesign.prediction2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 import edu.cmu.pocketsphinx.demo.R;
 
 public class SelectCardActivity extends AppCompatActivity {
@@ -16,5 +19,16 @@ public class SelectCardActivity extends AppCompatActivity {
         setSupportActionBar(instructionsToolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        GridView cardSelectionGrid = (GridView) findViewById(R.id.card_selection_grid);
+        cardSelectionGrid.setAdapter(new ImageAdapter(this));
+
+        cardSelectionGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(SelectCardActivity.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
